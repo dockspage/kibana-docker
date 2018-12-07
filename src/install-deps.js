c600073c4ea9b484775bddf2646e965b41e764b6
Anton <anton@adc.sh>
2018-12-07 12:31:59 +0300

    install-deps show where required

--- src/install-deps.js
+++ src/install-deps.js
@@ -1,8 +1,17 @@
 import spawn, { fork } from 'spawncommand'
+import { relative } from 'path'
 import { dependencies, devDependencies } from './kibana.json'
 
 const DD = { ...dependencies, ...devDependencies }
 
+const getFile = (err) => {
+  const [,file] = new RegExp(`(${process.cwd()}.+)\\)$`, 'm').exec(err) || []
+  if (!file) throw new Error('Cannot find the module where imported.')
+  const [path, line, col] = file.split(':')
+
+  const p = relative('', path)
+  return `${p}:${line}:${col}`
+}
 async function run() {
   const f = fork('src/cli', [], {
     stdio: 'pipe',
@@ -21,19 +30,22 @@ async function run() {
     f.promise,
     P,
   ])
-  const [,r] = /Cannot find module '(.+?)'/.exec(res.stderr) || []
+  let [,r] = /Cannot find module '(.+?)'/.exec(res.stderr) || []
+  const file = getFile(res.stderr)
   if (r) {
-    console.log('MISSING %s', r)
+    const rr = r.split('/', 1)
+    if (!r.startsWith('@')) r = rr
+    console.log(
+      'MISSING %s%s in %s', r, r != rr ? ` (import ${rr})` : '',
+      file,
+    )
     const version = DD[r]
     if (!version) throw new Error('Unknown version')
     const exact = `${r}@${version}`
-    // const a = await confirm(`Install ${exact}?`)
-    // if (!a) return
     console.log(' yarn add -E %s', exact)
-    const p = spawn('yarn', ['add', '-E', exact])
+    const p = spawn('yarn', ['add', '-E', exact], { cwd: 'kibana' })
     const rres = await p.promise
     if (rres.code != 0) throw new Error(rres.stderr)
-    // await confirm(`${r} installed. Continue?`)
     await run()
   }
 }
